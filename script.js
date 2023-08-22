185becc71f8c409bea176e004de863913614ab9e
Dinesh-Kumar-002 <dkdon812002@gmail.com>
2023-08-22 22:00:46 +0530

    10pmnight

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 new Glider(document.querySelector('.glider'), {
-    // Mobile-first defaults
    
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -9,10 +8,8 @@ new Glider(document.querySelector('.glider'), {
     draggable:true,
     responsive: [
       {
-        // screens greater than >= 775px
         breakpoint: 550,
         settings: {
-          // Set to `auto` and provide item width to adjust to viewport
           slidesToShow: 2,
           slidesToScroll: 1,
           itemWidth: 150,
@@ -20,11 +17,42 @@ new Glider(document.querySelector('.glider'), {
         }
       },
       {
-        // screens greater than >= 1024px
         breakpoint: 1024,
         settings: {
           slidesToShow: 4,
           slidesToScroll: 1,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      }
+    ]
+  });
+  document.onselectstart = (event) => {
+    event.preventDefault();
+  }
+  new Glider(document.querySelector('.glider2'), {
+    // Mobile-first defaults
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    draggable:true,
+    dots: '#resp-dots',
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 775,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 5,
+          slidesToScroll: 0.2,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      },{
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5.5,
+          slidesToScroll: 0.2,
           itemWidth: 150,
           duration: 0.25
         }
