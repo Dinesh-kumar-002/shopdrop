09d2764a91f34655f3bfa11a5802be4a43532390
Dinesh-Kumar-002 <dkdon812002@gmail.com>
2023-08-22 18:08:39 +0530

    220820023(06:08PM)

---
+++ script.js
@@ -0,0 +1,33 @@
+new Glider(document.querySelector('.glider'), {
+    // Mobile-first defaults
+   
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    scrollLock: true,
+    dots: '#dots',
+    rewind:true,
+    draggable:true,
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 550,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      },
+      {
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      }
+    ]
+  });
