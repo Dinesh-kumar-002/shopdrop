de19d7d9bb28653c527c902e3f17162c465b0e62
Dinesh-Kumar-002 <dkdon812002@gmail.com>
2023-08-24 10:13:32 +0530

    09:30am

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ new Glider(document.querySelector('.glider'), {
     scrollLock: true,
     dots: '#dots',
     rewind:true,
-    // draggable:true,
+    draggable:true,
     responsive: [
       {
         breakpoint: 550,
@@ -42,7 +42,7 @@ new Glider(document.querySelector('.glider'), {
     // Mobile-first defaults
     slidesToShow: 5,
     slidesToScroll: 1,
-    // draggable:true,
+    draggable:true,
     dots: '#resp-dots',
     responsive: [
       {
@@ -114,4 +114,77 @@ showmore.innerText="Show more";
 
 
 
+// addtocart
+var p_array=[];
+function addtocart(btn){
+  var parent=btn.closest(".trend");
+  var product_image=parent.querySelector('img').src;
+  var product_name=parent.querySelector('.text').innerText;
+  var id_value=parent.querySelector('input').value;
+  var product_price=parseFloat(parent.querySelector('.original').textContent.substring(1));
+  addtocartpage(product_image,product_name,product_price,id_value)
+}
+var realcart=document.querySelector('.realcart');
+function addtocartpage(p_image,p_name,p_price,p_id){
+  if(p_array.includes(p_id)){
+    return;
+  }
+  else{
+    p_array.push(p_id);
+    realcart.innerHTML+=`
+  <div class="products d-flex mt-2">
+                                <div class="product">
+                                    <img src="${p_image}" alt="">
+                                </div>
+                                <div class="nameandquantity d-flex flex-column justify-content-around">
+                                    <h6 class="text product-name">${p_name}</h6>
+                                    <h6 class="text product-id">${p_id}</h6>
+                                    <h6 class="text price-id" style="display:none;">${p_price}</h6>
+                                    <div class="quantity d-flex align-items-end" >
+                                    <button class="btn-secondary minus" onclick="minus(this)">-</button>
+                                    <input type="number" class="input ps-2" value="1">
+                                    <button class="btn-secondary add" onclick="add(this)">+</button>
+                                   </div>
+                                </div>
+                                <div class="individual-price d-flex justify-content-center align-items-center ms-5 ps-4">
+                                    ${p_price}
+                                </div>
+                            </div>
+  `;
+  total(p_price)
+  }
+  
+}
+var price=0;
+function total(p_price){
+  var displaying_total=document.querySelector('.total-value');
+  price+=p_price;
+  displaying_total.textContent=price;
 
+
+}
+function minus(btn){
+  if(btn.nextElementSibling.value==0){
+    btn.closest('.product').remove();
+  }
+  else{
+    var minusbtn=btn.nextElementSibling.value--;
+  subtotal(btn,minusbtn);
+  console.log(minusbtn);
+  }
+  
+}
+function add(btn){
+  var addbtn=btn.previousElementSibling.value++;
+  console.log(addbtn);
+  subtotal(btn,addbtn);
+}
+
+function subtotal(btn,val){
+  // debugger;
+  var subtotal_parent=btn.closest(".products");
+  var subtotal_id=parseInt(subtotal_parent.querySelector(".price-id").innerText);
+  var subtotal=subtotal_parent.querySelector(".individual-price");
+  subtotal.innerText=subtotal_id*val;
+  console.log(subtotal,subtotal_id,btn);
+}
